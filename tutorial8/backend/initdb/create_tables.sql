475b6e943cb4ff02e67202b16ae56785ba178d09
djamn <david@jamnig.net>
2024-12-10 13:39:48 +0100

    added docker compose

---
+++ tutorial8/backend/initdb/create_tables.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS Bikes
+
+CREATE TABLE Bike
+(
+    bike_id             SERIAL PRIMARY KEY,
+    bike_name           VARCHAR(100) NOT NULL,
+    is_available        BOOLEAN      DEFAULT TRUE,
+    size                INT          NOT NULL,
+    price               FLOAT        NOT NULL,
+);
+
+
+INSERT INTO Bike (bike_name, is_available, size, price)
+VALUES ('Mountain-Bike 300', TRUE, 20, 5.0),
+       ('E-Bike', FALSE, 50, 5.0),
+        ('Mountain-Bike 500', TRUE, 40, 10.0);
